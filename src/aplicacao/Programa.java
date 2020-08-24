f60b85ec453143a9d7957615374c478afdfeeb57
Dulcidio da Luz Sobrinho <dulcidio.sobrinho@gmail.com>
2020-08-23 22:26:40 -0300

    Projeto criado

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,21 @@
+/**
+ * @author Dulcidio Sobrinho
+ * 23 de ago de 2020
+ */
+package aplicacao;
+
+/**
+ * @author dlsob
+ *
+ */
+public class Programa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
