0f646a5ac5f93bee917aa614e05645ade23d3138
Dulcidio da Luz Sobrinho <dulcidio.sobrinho@gmail.com>
2020-09-01 01:31:00 -0300

    limpar tela e tratamento de excessoes

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -4,12 +4,13 @@
  */
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadres.PartidaDeXadres;
 import xadres.PecasXadres;
 import xadres.PosicaoXadres;
-import xadres.XadresPosicao;
+import xadres.XadresExcessao;
 
 /**
  * @author dlsob
@@ -26,15 +27,26 @@ public class Programa {
 		PartidaDeXadres partida  =  new PartidaDeXadres();
 		
 		while(true) {
-		UI.printTabuleiro(partida.getPecas());
-		System.out.println(" ");
-		System.out.println("Origem: ");
-		PosicaoXadres origem = UI.lerPosicaoXadres(sc);
-		
-		System.out.println(" ");
-		System.out.println("Destino: ");
-		PosicaoXadres destino = UI.lerPosicaoXadres(sc);
-		
+			try {
+				UI.limparTela();	
+				UI.printTabuleiro(partida.getPecas());
+				System.out.println(" ");
+				System.out.println("Origem: ");
+				PosicaoXadres origem = UI.lerPosicaoXadres(sc);
+				
+				System.out.println(" ");
+				System.out.println("Destino: ");
+				PosicaoXadres destino = UI.lerPosicaoXadres(sc);
+			}
+			catch(XadresExcessao e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			
 		
 		
 		PecasXadres pecaCapturada = partida.realizarMovimentoPeca(origem, destino);
